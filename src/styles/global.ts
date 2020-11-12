0399abbb58091209cbf7bc437e0638c12290e750
Paulo Carvalho <pauloej.carvalho@gmail.com>
2020-11-12 19:18:10 -0300

    feat(global-styles): including github logo background and minor changes at global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
+import githubBackground from '../assets/github_logo.svg';
+
 export default createGlobalStyle`
   * {
     margin: 0;
@@ -9,11 +11,22 @@ export default createGlobalStyle`
   }
 
   body {
-    background: #F0F0F5;
+    background: #F0F0F5 url(${githubBackground}) no-repeat 70% top;
     -webkit-font-smoothing: antialiased;
+
   }
 
   body, input, button {
     font: 16px Roboto, sans-serif;
+  }
+
+  #root {
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 40px 20px;
+  }
+
+  button {
+    cursor: pointer;
   }
 `;
