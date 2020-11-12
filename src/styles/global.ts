bf5d5330c5e5f1f503f582f82dbd61457f8ad7c5
Paulo Carvalho <pauloej.carvalho@gmail.com>
2020-11-12 19:08:26 -0300

    feat(styled-components): adding styled components dependency and creating global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,15 @@
+import { create } from 'domain';
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #F0F0F5;
+  }
+`;
