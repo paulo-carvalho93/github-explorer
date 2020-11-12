343df090df726c7e0255d4fd451c5faacbc852d9
Paulo Carvalho <pauloej.carvalho@gmail.com>
2020-11-12 19:35:31 -0300

    feat(dashboard): adding input and button with styled component and polished

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositories on GitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositories on GitHub</Title>
+
+      <Form action="">
+        <input placeholder="Type the name of the repository" />
+        <button type="submit">Search</button>
+      </Form>
+    </>
+  );
 };
 
 export default Dashboard;
