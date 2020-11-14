ecf966bc2617ea22eabf2a9e7e8409ea0c7ffa65
Paulo Carvalho <pauloej.carvalho@gmail.com>
2020-11-14 14:50:52 -0300

    feat(dashboard): styling Dashboard and adding Icon Arrow with React Icons

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
@@ -14,6 +15,21 @@ const Dashboard: React.FC = () => {
         <input placeholder="Type the name of the repository" />
         <button type="submit">Search</button>
       </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/24879695?s=460&u=0b4e53a56faab57c7b2b8a766261b13a5c196425&v=4"
+            alt="Paulo Carvalho"
+          />
+          <div>
+            <strong>paulo-carvalho93/github-explorer</strong>
+            <p>Github Explorer</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
